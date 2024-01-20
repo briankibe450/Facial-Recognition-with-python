f980c358a3213ca4905af2a86f1a796b4c9139ea
briankibe450 <138442956+briankibe450@users.noreply.github.com>
2024-01-20 13:57:26 +0300

    Update AwesomePoseEstimationProject.py

--- AwesomePoseEstimationProject.py
+++ AwesomePoseEstimationProject.py
@@ -17,7 +17,7 @@ while True:
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PN, 3,
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
                 (255, 0, 0), 3)
 
     cv2.imshow("Image", img)
